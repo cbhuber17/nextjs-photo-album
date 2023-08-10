38f59f5948be79ee7f18ce015903f24658025f7b
Colin Huber <cbhuber@gmail.com>
2023-08-10 16:50:49 -0600

    feat: display image when uploaded.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,36 @@
 "use client";
 
 import Image from "next/image";
-import { CldUploadButton } from "next-cloudinary";
+import { CldUploadButton, CldImage } from "next-cloudinary";
+import { useState } from "react";
+
+type UploadResult = {
+  info: {
+    public_id: string;
+  };
+
+  event: "success";
+};
 
 export default function Home() {
+  const [imageId, setImageId] = useState("");
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <CldUploadButton uploadPreset="unlssvf6" />
+      <CldUploadButton
+        onUpload={(result: UploadResult) => setImageId(result.info.public_id)}
+        uploadPreset="unlssvf6"
+      />
+
+      {imageId && (
+        <CldImage
+          width="400"
+          height="300"
+          src={imageId}
+          sizes="100vw"
+          alt="Young girl ballet dancing"
+        />
+      )}
     </main>
   );
 }
